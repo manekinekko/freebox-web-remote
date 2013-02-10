a58c0ac9c6cbf87ac214b4c7c63ba00ddaeefb46
manekinekko <maneki.nekko@gmail.com>
2013-02-10 21:39:26 +0100

    added long press support

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 /*jslint nomen:true,sloppy:false,plusplus:true,devel:true,validthis:true*/
 /*globals document:true,localStorage:true,XMLHttpRequest:true*/
 (function () {
-	"use strict";
+	'use strict';
 
-	function xhr(remote_id, key, onload, onerror) {
-		var xhr_ = new XMLHttpRequest();
-		xhr_.open("GET", "http://hd1.freebox.fr/pub/remote_control?code=" + remote_id + "&key=" + key, true);
-		xhr_.onload = onload || function () {};
-		xhr_.onerror = onerror || function () {};
-		xhr_.send();
+	var long_press_timer = null, long_press_delay = null;
+
+	function send_xhr(remote_id, key, onload, onerror) {
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', 'http://hd1.freebox.fr/pub/remote_control?code=' + remote_id + '&key=' + key, true);
+		xhr.onload = onload || function () {};
+		xhr.onerror = onerror || function () {};
+		xhr.send();
 	}
 
 	function check_remote_id(remote_id, onsuccess, onerror) {
-		xhr(remote_id, "blue", function (e) {
+		xhr(remote_id, 'blue', function (e) {
 			(200===this.status) ? onsuccess(remote_id) : onerror();
 		});
 	}
@@ -30,23 +32,41 @@
 		});
 	}
 
-	function send_xhr(e) {
+	function run_key(e) {
 		e.preventDefault();
-		var remote_id = localStorage.getItem('remote_id'),
-			key = this.getAttribute("href");
-			key = key.replace("#key-", "");
-		xhr(remote_id, key);
+		var long_keys = ['vol_inc', 'vol_dec', 'up', 'down', 'left', 'right'],
+			remote_id = localStorage.getItem('remote_id'),
+			key = this.getAttribute('data-key');
+
+		send_xhr(remote_id, key);
+
+		if (long_keys.indexOf(key) != -1) {
+			long_press_delay = setTimeout(function () {
+				long_press_timer = setInterval(function () {
+					send_xhr(remote_id, key);
+				}, 250);
+			}, 250);
+		}
+
 	}
 
-	var i,
+	function clear_long_press() {
+		clearInterval(long_press_timer);
+		clearTimeout(long_press_delay);
+	}
+
+	(function init () {
+		var i,
 		remote_id_dom = document.querySelector('#remote-id'),
-		keys_dom = document.querySelectorAll('a[href^="#key-"]');
-	remote_id_dom.value = localStorage.getItem('remote_id')  || "";
-	remote_id_dom.addEventListener('keyup', store_remote_id);
-	remote_id_dom.addEventListener('change', store_remote_id);
+		keys_dom = document.querySelectorAll('a[data-key]');
+		remote_id_dom.value = localStorage.getItem('remote_id')  || '';
+		remote_id_dom.addEventListener('keyup', store_remote_id);
+		remote_id_dom.addEventListener('change', store_remote_id);
 
-	for (i = 0; i < keys_dom.length; i++) {
-		keys_dom[i].addEventListener("click", send_xhr);
-	}
+		for (i = 0; i < keys_dom.length; i++) {
+			keys_dom[i].addEventListener('mousedown', run_key);
+			keys_dom[i].addEventListener('mouseup', clear_long_press);
+		}
+	}());
 
 }());
